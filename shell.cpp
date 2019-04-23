ee6268110c4b397b29c0cc8d4e14313d68f0627d
professordeng <2248262060@qq.com>
2019-04-23 17:49:38 +0800

    实现 shell

--- shell.cpp
+++ shell.cpp
@@ -1,18 +1,17 @@
 #include <stdio.h>
-#include <cstdlib>
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
-
-// 
+ 
 void type_prompt() {
 	static int first_time = 1;
-	if ( first_time ) {       // clear screen for the first time
+	if ( first_time ) {       // 清除屏幕
 		const char* CLEAR_SCREEN_ANSI = " \e[1;1H\e[2J";
 		write(STDOUT_FILENO, CLEAR_SCREEN_ANSI,12);
 		first_time = 0;
 	}
-	printf("#");              //display
+	printf("#");              // 显示
 }
 
 void read_command ( char cmd[], char *par[] ) {
@@ -20,7 +19,7 @@ void read_command ( char cmd[], char *par[] ) {
 	int count = 0, i = 0, j = 0;
 	char *array[100], *pch;
 
-	// Read one line
+	// 读一行字符
 	for(;;) {
 		int c = fgetc ( stdin );
 		line[count++] = (char) c;
@@ -29,36 +28,39 @@ void read_command ( char cmd[], char *par[] ) {
 	if ( count == 1 ) return;
 	pch = strtok ( line, " \n");
 
-	// parse the line into words
+	// 将一行解析为单词
 	while ( pch != NULL ) {
 		array[i++] = strdup ( pch );
 		pch = strtok ( NULL, " \n");
 	}  
-	// first word is the command
+	// 第一个单词就是指令
 	strcpy ( cmd, array[0] );
 
-	// others are parameters
+	// 其他单词就是参数
 	for ( j = 0; j < i; j++ )
 		par[j] = array[j];
-	par[i] = NULL; //NULL-terminate the parameter list
+	par[i] = NULL; // NULL 表示参数的结束
 }
 
 int main(){
+	// 一个命令包括命令和参数
 	char cmd[100], command[100], *parameters[20];  
-	// environment variable 
+	// 环境变量，调用可执行程序
 	char *envp[] = { (char *) "PATH=/bin", 0 };
-	while ( 1 ) {         // repeat forever
-		type_prompt();    // display prompt on screen
-		read_command ( command, parameters ); // read input from terminal
-		if ( fork() != 0 ) //parent
-			wait ( NULL ); //wait for child
+	while ( 1 ) {                               // 进入是循环
+		type_prompt();                          // 显示指令
+		read_command ( command, parameters );   // 从终端读取指令
+		if ( fork() != 0 )                      // 父进程阻塞
+			wait ( NULL );                      // 等待子进程
 		else {
-			strcpy ( cmd, "/bin/" );
-			strcat ( cmd, command );
-			execve ( cmd, parameters, envp ); //execute command 
+			strcpy ( cmd, "/bin/" );            // 环境变量 
+			strcat ( cmd, command );            // 拼接程序路径
+			execve ( cmd, parameters, envp );   // 执行命令
 		}
-		if ( strcmp ( command, "exit" ) == 0 )
+		if ( strcmp ( command, "exit" ) == 0 )  // 如果第一个单词是 exit ，那就退出程序 
 			break;
+		if ( strcmp ( command, "clear" ) == 0 ) // 我喜欢 clear
+			system("echo hello world!");			
 	}
 	return 0;
 }
